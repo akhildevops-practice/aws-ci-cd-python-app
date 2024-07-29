aa2e869ba59512b9675e5d1309749d1f8b9f69e9
akhilprabhu20 <117083761+akhilprabhu20@users.noreply.github.com>
2024-07-29 11:51:45 +0530

    stop and start

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull akhilprabhu2005/aws-simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:8000 akhilprabhu2005/aws-simple-python-flask-app
+docker run -d -p 5000:5000 akhilprabhu2005/aws-simple-python-flask-app
